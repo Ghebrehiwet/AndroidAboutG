24c85b29eba917da309092666b95d59b90dd8f33
Ghebre <geberhane@gmail.com>
2017-04-20 00:28:27 -0700

    adds a new screen to the app and passing information to it from the main screen
    1. Adds a new screen to the app and passing information to it from the
    main screen.
    2. In the second activity, adds methods to log debug information for
    each lifecycle event.

---
+++ app/src/main/java/com/example/fiori/aboutme/SecondActivity.java
@@ -0,0 +1,68 @@
+package com.example.fiori.aboutme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
+
+    /** Called when the activity is first created. */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textViewReveiver);
+        textView.setText("This text has been brought from the main activity: " + message);
+
+        Log.d(TAG, "The onCreate() event");
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "The onDestroy() event");
+    }
+
+
+
+}
