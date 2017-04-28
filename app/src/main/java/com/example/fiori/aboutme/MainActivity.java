7ba6ee474efac87b1577fdd96aa7e2f6b3657be9
Ghebre <geberhane@gmail.com>
2017-04-27 17:12:10 -0700

    Adding Layouts
    Add a 2x2 grid of buttons to the main screen, where each button displays
    a 'toast' when tapped.
    
    When the top-left cell is tapped, take the user to a new screen that
    displays a vertically scrolling list of text information. Each list row
    should have 2+ pieces of information (e.g. book title & author) in
    separate views. The list uses a RecyclerView to display more items than
    can fit on the screen at once.

--- app/src/main/java/com/example/fiori/aboutme/MainActivity.java
+++ app/src/main/java/com/example/fiori/aboutme/MainActivity.java
@@ -4,16 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static String EXTRA_MESSAGE = "com.example.fiori.aboutme";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                if(position==0){
+                    startActivity(new Intent(MainActivity.this, NewActivity.class));
+                    Toast.makeText(MainActivity.this, "" + "Item : " + (position + 1) + "Take me to another activity",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "" + "Item : " + (position + 1),
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     public void sendMessage(View view) {
@@ -22,5 +48,22 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+
+
     }
+
+//    public void goToActivity2(View view) {
+//        Intent intent = new Intent(this, NewActivity.class);
+////        EditText editText = (EditText) findViewById(R.id.editTextEnterText);
+////        String message = editText.getText().toString();
+////        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//
+//
+//
+//    }
+
+
+
 }
