90291a453a592f615e4cf53f12bb726edae17f10
Ghebre <geberhane@gmail.com>
2017-05-04 01:55:24 -0700

    adds functionalities such as menu, toolbar, dialog
    1) Creates an About screen with brief information about the app,
    
    2) Adds Toolbar (or ActionBar) navigation to the  application, with menu
    options for the RecyclerView screen created last week, new About screen,
    and any other top-level screen in the application.
    
    3) Enables navigation from the home page to the screens listed in the
    app bar menu
    
    4) Enables the app bar in each activity and 'up' navigation in each
    child page
    
    5) Shows the options menu on child activities, but with the option for
    the current page hidden,
    add a 'Search' option in your Toolbar
    ,
    6) Uses an array of choices in dialog and logs which option was
    selected,

--- app/src/main/java/com/example/fiori/aboutme/NewActivity.java
+++ app/src/main/java/com/example/fiori/aboutme/NewActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.widget.RelativeLayout;
 
@@ -18,10 +21,10 @@ public class NewActivity extends AppCompatActivity {
     RecyclerView.LayoutManager recylerViewLayoutManager;
     String[][] subjects =
             {
-                    { "ANDROID", "1" },
-                    { "PHP", "2" },
-                    { "JSON", "3" },
-                    { "SWIFT", "4" },
+                    { "ANDROID", "Android is a mobile operating system developed by Google, based on the Linux kernel and designed primarily for touchscreen mobile devices such as smartphones and tablets" },
+                    { "PHP", "PHP (recursive acronym for PHP: Hypertext Preprocessor) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML." },
+                    { "JSON", "JSON, or JavaScript Object Notation, is a minimal, readable format for structuring data. It is used primarily to transmit data between a server and web application, as an alternative to XML." },
+                    { "SWIFT", "Swift is a powerful and intuitive programming language for macOS, iOS, watchOS and tvOS" },
                     { "OBJECTIVE-C", "5" },
                     { "SQL", "" },
                     { "JAVA", "" },
@@ -43,11 +46,12 @@ public class NewActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_new);
 
-//        recyclerView.setHasFixedSize(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         context = getApplicationContext();
-
-//        mLinearLayoutManager = (RelativeLayout) findViewById(R.id.relativelayout1);
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview1);
 
@@ -58,5 +62,17 @@ public class NewActivity extends AppCompatActivity {
         // specify an adapter
         recyclerViewAdapter = new RecyclerViewAdapter(context, subjects);
         recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        // hide menu item for this page
+        MenuItem itemToHide = menu.findItem(R.id.action_recycler);
+        itemToHide.setVisible(false);
+
+        return true;
     }
 }
